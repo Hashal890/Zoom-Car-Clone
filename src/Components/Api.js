c83b66fc06c5b622d0687f9041fc882565f17027
Harshal Pardeshi <100831868+Hashal890@users.noreply.github.com>
2022-12-11 19:02:26 +0530

    Added new link.

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -2,19 +2,19 @@ import axios from "axios";
 
 export const GetData = (number) => {
   return axios.get(
-    `https://harshal-json-mock-server.herokuapp.com/users?number=${number}`
+    `https://jsonmockserver.cyclic.app/user?number=${number}`
   );
 };
 
 export const PostData = async ({ name, email, number, password }) => {
-  // return axios.post("https://harshal-json-mock-server.herokuapp.com/users", {
+  // return axios.post("https://jsonmockserver.cyclic.app/user", {
   //   name: name,
   //   email: email,
   //   number: number,
   //   password: password,
   // });
   let data = await fetch(
-    "https://harshal-json-mock-server.herokuapp.com/users",
+    "https://jsonmockserver.cyclic.app/user",
     {
       method: "POST",
       body: JSON.stringify({ name, email, number, password }),
